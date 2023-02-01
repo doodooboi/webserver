6a50d759710580f3e8a717bc4918a28a5d56aa48
ytugy <65300639+doodooboi@users.noreply.github.com>
2023-01-31 23:46:49 -0600

    Update login.js

--- client/login.js
+++ client/login.js
@@ -1,8 +1,10 @@
-const ip = "https://doodooboi-redesigned-umbrella-7w794jxqx5v2w57g-1820.preview.app.github.dev/auth-api/"
+const ip = "https://caca.onthewifi.com/auth/"
 
 function login() {
   const user = document.getElementById("user")
   const pass = document.getElementById("pass")
+  const statusBox = document.getElementById("status")
+  var style = getComputedStyle(document.body)
   
   const xhttp = new XMLHttpRequest();
   xhttp.open("POST", ip + "login", true);
@@ -13,9 +15,28 @@ function login() {
   }));
 
   xhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
+    if (this.readyState == 4) {
       const objects = JSON.parse(this.responseText);
-      console.log(objects);
+
+      statusBox.style.display = "flex"
+      statusBox.style.opacity = 100
+
+      setTimeout(() => {
+        statusBox.style.display = "none"
+        statusBox.style.opacity = 0
+      }, 1500);
+
+      if (this.status == 403) {
+        statusBox.style.backgroundColor = style.getPropertyValue("--error")
+        statusBox.style.borderColor = style.getPropertyValue("--errorborder")
+      } else if (this.status === 200) {
+        statusBox.style.backgroundColor = style.getPropertyValue("--success")
+        statusBox.style.borderColor = style.getPropertyValue("--successborder")
+      }
+
+      statusBox.innerHTML = "<p><span class='response'>" + this.status.toString() + ":</span> " + objects.response + "</p>"
     }
   };
+
+  return false;
 }
